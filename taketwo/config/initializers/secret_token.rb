07a902c8b3a938647cc5754ada8d306b565123be
warrencurtis <warpaint7@yahoo.com>
2013-08-26 22:21:01 -0700

    new files

---
+++ taketwo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Taketwo::Application.config.secret_key_base = '7d37699f3ec781e8b9c381363960e188cf95c2a27ae9a43c04c387a79b5d5af677c0e830b209a20758b7df79dac261346a7454ba7a4ae1e612efb8a09a88523e'
